c585bcd2a9719250a59eaa3d90eae36df203c9c3
Seng Sopheng <phengg1405@gmail.com>
2023-12-06 23:47:46 +0700

    clickable text component and Checkbox component

--- app/src/main/java/com/example/gymapp/components/AppComponents.kt
+++ app/src/main/java/com/example/gymapp/components/AppComponents.kt
@@ -1,13 +1,17 @@
 package com.example.gymapp.components
 
+import android.util.Log
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -17,13 +21,16 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -31,6 +38,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 //import androidx.compose.material.icons.filled.Visibility
 //import androidx.compose.material.icons.filled.VisibilityOff
 
@@ -164,4 +172,66 @@ fun PasswordTextFieldComponent(
         visualTransformation = if (passwordVisible.value) VisualTransformation.None else PasswordVisualTransformation(),
         isError = !errorStatus
     )
+}
+
+@Composable
+fun ClickableTextComponent(value: String, onTextSelected: (String) -> Unit) {
+    val initialText = "By continuing you accept our "
+    val privacyPolicyText = "Privacy Policy"
+    val andText = " and "
+    val termsAndConditionsText = "Term of Use"
+
+    val annotatedString = buildAnnotatedString {
+        append(initialText)
+        withStyle(style = SpanStyle(color = Primary)) {
+            pushStringAnnotation(tag = privacyPolicyText, annotation = privacyPolicyText)
+            append(privacyPolicyText)
+        }
+        append(andText)
+        withStyle(style = SpanStyle(color = Primary)) {
+            pushStringAnnotation(tag = termsAndConditionsText, annotation = termsAndConditionsText)
+            append(termsAndConditionsText)
+        }
+    }
+
+    ClickableText(text = annotatedString, onClick = { offset ->
+
+        annotatedString.getStringAnnotations(offset, offset)
+            .firstOrNull()?.also { span ->
+                Log.d("ClickableTextComponent", "{${span.item}}")
+
+                if ((span.item == termsAndConditionsText) || (span.item == privacyPolicyText)) {
+                    onTextSelected(span.item)
+                }
+            }
+
+    })
+}
+
+
+@Composable
+fun CheckboxComponent(
+    value: String,
+    onTextSelected: (String) -> Unit,
+    onCheckedChange: (Boolean) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(56.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+
+        val checkedState = remember {
+            mutableStateOf(false)
+        }
+
+        Checkbox(checked = checkedState.value,
+            onCheckedChange = {
+                checkedState.value = !checkedState.value
+                onCheckedChange.invoke(it)
+            })
+
+        ClickableTextComponent(value = value, onTextSelected)
+    }
 }
