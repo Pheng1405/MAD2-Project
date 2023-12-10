633af6128b3ad25f30d2d9a34dfc5da1fdf8bd61
SONGMENG Panhareach <panhareach.songmeng@allweb.com.kh>
2023-12-10 17:31:28 +0700

    add search bar

--- app/src/main/java/com/example/gymapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/gymapp/screens/HomeScreen.kt
@@ -1,15 +1,22 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.gymapp.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -63,7 +70,25 @@ fun HomeScreen(homeViewModel: HomeViewModel = viewModel(), navController: NavCon
                 .padding(paddingValues)
         ) {
             Column(modifier = Modifier.fillMaxSize()) {
+                var query by remember {
+                    mutableStateOf("")
+                }
+                var active by remember {
+                    mutableStateOf(false)
+                }
+                SearchBar(
+                    query = query,
+                    onQueryChange = {
+                                    query = it
+                    },
+                    onSearch = {
+                          active = false
+                    },
+                    active = active,
+                    onActiveChange = { active = it }
+                ) {
 
+                }
 
             }
 
