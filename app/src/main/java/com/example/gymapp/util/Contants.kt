a22a2994a80f82b4f30b461b3b83ea17172e0889
Seng Sopheng <phengg1405@gmail.com>
2023-12-23 14:49:17 +0700

    create movie getter constant

--- app/src/main/java/com/example/gymapp/util/Contants.kt
+++ app/src/main/java/com/example/gymapp/util/Contants.kt
@@ -3,6 +3,7 @@ package com.example.gymapp.util
 object Constants {
     const val BASE_URL = "https://mc-api.flexibiz.tech"
     const val IMAGE_BASE_URL = "https://mc-api.flexibiz.tech/upload/image/"
+    const val VIDEO_BASE_URL = "https://mc-api.flexibiz.tech/upload/video/"
     const val ACCESS_TOKEN = "accessToken"
     const val REFRESH_TOKEN = "refreshToken"
     const val  DATASTORE_NAME = "TEST"
