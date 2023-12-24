ae8853d0b22efec74b9b7ddd6633189eefc989e9
Seng Sopheng <phengg1405@gmail.com>
2023-12-24 14:47:40 +0700

    add movie genres dto when getting movie detail movie

--- app/src/main/java/com/example/gymapp/domain/model/Movie.kt
+++ app/src/main/java/com/example/gymapp/domain/model/Movie.kt
@@ -3,6 +3,16 @@ package com.example.gymapp.domain.model
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class CategoryModel(
+    val id : String,
+    val category_id : String
+)
+
+@Serializable
+data class CategoriesModel(
+    val categories : List<CategoryModel>
+)
+@Serializable
 data  class MovieModel (
     val id : String,
     val title : String,
@@ -11,6 +21,7 @@ data  class MovieModel (
     val land_thumbnail : String,
     val rating : Double,
     val link : String,
+    val category : CategoriesModel,
 )
 
 @Serializable
@@ -22,3 +33,4 @@ data  class MovieDto (
 data class AllMovieModel(
     val data : List<MovieModel>,
 )
+
